7272819dc277fc83cd2337ceb5aabf37362ed965
Oleksiy_Taranenko <oleksiytaranenko01@gmail.com>
2021-04-06 18:33:30 +0300

    homework 2

---
+++ js/script.js
@@ -0,0 +1,31 @@
+'Hi teacher,' + ' it\'s me' + ' again:)'; //Hi teacher, it's me again:)
+
+'Dnipro' + 19 + 18; //Dnipro1918
+
+'' + undefined + true; //undefinedtrue
+
+-0 || 'Karl X' && null && ''; //null
+
+true && NaN || -999 && 'don\'t stop me now' || 0; //don't stop me now
+
+!1 || !!'please continue' || 0 && undefined || -123 || !!false; //true
+
+!!(false || 123) && !('blue sky' || false); //false
+
+!null || !1 || !'cloudy sky' && !!undefined || !!true; //true
+
+'123' > 321 && null == 0; //false
+
+!!777 <= 888 || !false == '0'; //true
+
+'123a' * 0 / !123 + '123'; //NaN123
+
+!!true + !!false + 'null' + ('36' / 6); //1null6
+
+'Terminator' > 'Batman' || 'Joker' > 'spiderman'; //true
+
+'999' >= 999 || null != 0 && true === 'true'; //true
+
+'my homework is over' || '-123' || true && -0 || undefined; //my homework is over
+
+0 || 'hello' && 'bye bye' || 1; //bye bye
